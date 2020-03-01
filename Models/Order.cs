038ac7725ed619c79e84b429f513deed06fb2731
kathrindemel <61613415+kathrindemel@users.noreply.github.com>
2020-03-01 14:44:40 +0100

    Add files via upload

---
+++ Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using SchwimmbadLib;
+
+namespace SchwimmbadWebApplication.Models
+{
+    public class Order
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int BuchungId { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public int SchwimmbahnId { get; set; }
+        public Buchung Buchung { get; set; }
+        public Schwimmbahn Schwimmbahn { get; set; }
+    }
+}
